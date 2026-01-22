a9ba0731bf8780e5294ae30acda39fdbe088f945
gilaless06ss <gilalessrn@gmail.com>
2026-01-22 13:51:55 -0600

    si

--- src/app/features/pokedex/pokedex.ts
+++ src/app/features/pokedex/pokedex.ts
@@ -3,17 +3,26 @@ import { CommonModule } from '@angular/common';
 import { PokeapiService } from '../../core/services/pokeapi';
 import { SidebarComponent } from '../../shared/sidebar/sidebar';
 import { forkJoin } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pokedex',
   standalone: true,
-  imports: [CommonModule, SidebarComponent],
+  imports: [CommonModule, SidebarComponent, FormsModule],
   templateUrl: './pokedex.html',
   styleUrl: './pokedex.scss',
 })
 export class PokedexComponent implements OnInit {
   pokemons: any[] = [];
+  allPokemons: any[] = [];
   loading = false;
+
+  searchTerm = '';
+  suggestions: any[] = [];
+  showSuggestions = false;
+
+  showGrid = true;
+  showSidebar = false;
 
   constructor(private pokeApi: PokeapiService) {}
 
@@ -30,6 +39,7 @@ export class PokedexComponent implements OnInit {
     );
 
     forkJoin<any[]>(requests).subscribe(data => {
+      this.allPokemons = data;
       this.pokemons = data;
       this.loading = false;
     });
@@ -55,5 +65,47 @@ export class PokedexComponent implements OnInit {
         this.loading = false;
       });
     });
+  }
+
+  searchPokemon() {
+    const value = this.searchTerm.toLowerCase().trim();
+
+    if (!value) {
+      this.pokemons = this.allPokemons;
+      this.suggestions = [];
+      this.showSuggestions = false;
+      return;
+    }
+    
+    this.pokemons = this.allPokemons.filter(pokemon =>
+      pokemon.name.includes(value) ||
+      pokemon.id.toString() === value
+    );
+
+    this.suggestions = [];
+    this.showSuggestions = false;
+  }
+
+  updateSuggestions() {
+    const value = this.searchTerm.toLowerCase().trim();
+
+    if (value.length < 2) {
+      this.suggestions = [];
+      this.showSuggestions = false;
+      return;
+    }
+
+    this.suggestions = this.allPokemons.filter(pokemon =>
+      pokemon.name.includes(value)
+    ).slice(0, 5);
+
+    this.showSuggestions = this.suggestions.length > 0;
+  }
+
+  selectSuggestion(pokemon: any) {
+    this.searchTerm = pokemon.name;
+    this.pokemons = [pokemon];
+    this.suggestions = [];
+    this.showSuggestions = false;
   }
 }
